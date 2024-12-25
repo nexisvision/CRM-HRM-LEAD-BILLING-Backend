553da565744edba04645a970000e64909f0f6307
yash-babariya <yashbabariya549@gmail.com>
2024-12-25 12:16:04 +0530

    removing extra frontend folder

--- frontend/src/mock/data/authData.js
+++
@@ -1,9 +0,0 @@
-export const signInUserData = [
-    {
-        id: '21',
-        avatar: '/img/avatars/thumb-1.jpg',
-        userName: 'Charlie Howard',
-        email: 'user1@themenate.net',
-        password: '2005ipo'
-    }
-]
