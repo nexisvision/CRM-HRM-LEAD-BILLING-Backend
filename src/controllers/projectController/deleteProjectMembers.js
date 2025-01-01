05ee5b3b9242b3774d4d9ef4b3d21d11978e18de
Hacke0011 <palakardeshna01@gmail.com>
2025-01-01 14:50:49 +0530

    project ma changesdsdsd

--- src/controllers/projectController/deleteProjectMembers.js
+++ src/controllers/projectController/deleteProjectMembers.js
@@ -9,13 +9,15 @@ export default {
             id: Joi.string().required()
         }),
         body: Joi.object({
-            memberIds: Joi.array().items(Joi.string()).required()
+            project_members: Joi.object({
+                project_members: Joi.array().items(Joi.string()).required()
+            }).required()
         })
     }),
     handler: async (req, res) => {
         try {
             const { id } = req.params;
-            const { memberIds } = req.body;
+            const memberIdsToDelete = req.body.project_members.project_members;
 
             const project = await Project.findByPk(id);
 
@@ -24,35 +26,32 @@ export default {
             }
 
             // Parse project_members if it's a string
-            const projectMembers = typeof project.project_members === 'string' 
+            const currentProjectMembers = typeof project.project_members === 'string' 
                 ? JSON.parse(project.project_members) 
                 : project.project_members;
 
-            // Ensure we have an array to work with
-            const currentMembers = projectMembers?.members || [];
+            // Get current members array
+            const currentMembers = currentProjectMembers?.project_members || [];
 
             // Check if all memberIds to delete exist in the project
-            const nonExistingMembers = memberIds.filter(id => !currentMembers.includes(id));
+            const nonExistingMembers = memberIdsToDelete.filter(id => !currentMembers.includes(id));
             if (nonExistingMembers.length > 0) {
                 return responseHandler.error(res, `Some members not found in project: ${nonExistingMembers.join(', ')}`);
             }
 
             // Create new array without the specified members
-            const updatedMembers = [];
-            for (let member of currentMembers) {
-                if (!memberIds.includes(member)) {
-                    updatedMembers.push(member);
-                }
-            }
+            const updatedMembers = currentMembers.filter(
+                memberId => !memberIdsToDelete.includes(memberId)
+            );
 
             // Update the project with the new members list
             await project.update({
-                project_members: { members: updatedMembers },
+                project_members: { project_members: updatedMembers },
                 updated_by: req.user?.username
             });
 
             responseHandler.success(res, "Project members deleted successfully", {
-                project_members: { members: updatedMembers }
+                project_members: { project_members: updatedMembers }
             });
         } catch (error) {
             console.log(error);
@@ -60,3 +59,72 @@ export default {
         }
     }
 };
+
+
+
+
+
+
+
+// import Joi from "joi";
+// import Project from "../../models/projectModel.js";
+// import validator from "../../utils/validator.js";
+// import responseHandler from "../../utils/responseHandler.js";
+
+// export default {
+//     validator: validator({
+//         params: Joi.object({
+//             id: Joi.string().required()
+//         }),
+//         body: Joi.object({
+//             memberIds: Joi.array().items(Joi.string()).required()
+//         })
+//     }),
+//     handler: async (req, res) => {
+//         try {
+//             const { id } = req.params;
+//             const { memberIds } = req.body;
+
+//             const project = await Project.findByPk(id);
+
+//             if (!project) {
+//                 return responseHandler.notFound(res, "Project not found");
+//             }
+
+//             // Parse project_members if it's a string
+//             const projectMembers = typeof project.project_members === 'string' 
+//                 ? JSON.parse(project.project_members) 
+//                 : project.project_members;
+
+//             // Ensure we have an array to work with
+//             const currentMembers = projectMembers?.members || [];
+
+//             // Check if all memberIds to delete exist in the project
+//             const nonExistingMembers = memberIds.filter(id => !currentMembers.includes(id));
+//             if (nonExistingMembers.length > 0) {
+//                 return responseHandler.error(res, `Some members not found in project: ${nonExistingMembers.join(', ')}`);
+//             }
+
+//             // Create new array without the specified members
+//             const updatedMembers = [];
+//             for (let member of currentMembers) {
+//                 if (!memberIds.includes(member)) {
+//                     updatedMembers.push(member);
+//                 }
+//             }
+
+//             // Update the project with the new members list
+//             await project.update({
+//                 project_members: { members: updatedMembers },
+//                 updated_by: req.user?.username
+//             });
+
+//             responseHandler.success(res, "Project members deleted successfully", {
+//                 project_members: { members: updatedMembers }
+//             });
+//         } catch (error) {
+//             console.log(error);
+//             responseHandler.error(res, error.message);
+//         }
+//     }
+// };
