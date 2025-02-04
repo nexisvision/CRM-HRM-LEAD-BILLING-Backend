e87bc854a4c8a53ada0287b3c156e36ec1d11f0a
srushti <srushtikathiriya2003@gmail.com>
2025-02-04 16:55:23 +0530

    sales credit notes

--- src/controllers/salesCreditnoteControllers/getAllSalesCreditnote.js
+++ src/controllers/salesCreditnoteControllers/getAllSalesCreditnote.js
@@ -16,6 +16,7 @@ export default {
             const { id } = req.user;
             const salesCreditnote = await SalesCreditnote.findAll({ where: { related_id: id } });
             return responseHandler.success(res, "salesCreditnote fetched successfully", salesCreditnote);
+
         } catch (error) {
             return responseHandler.error(res, error?.message);
         }
