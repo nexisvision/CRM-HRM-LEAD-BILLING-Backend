025639687d8dfd9e11e5aab87509c7ff78456154
palakgrewox <palakgrewox@gmail.com>
2025-02-21 12:16:54 +0530

    21-02 -update

--- src/controllers/userControllers/getCreatedUsers.js
+++ src/controllers/userControllers/getCreatedUsers.js
@@ -1,7 +1,9 @@
 import Joi from "joi";
 import Users from "../../models/userModel.js";
+import Role from "../../models/roleModel.js";
 import validator from "../../utils/validator.js";
 import responseHandler from "../../utils/responseHandler.js";
+import { Op } from "sequelize";
 
 export default {
     validator: validator({
@@ -12,11 +14,50 @@ export default {
     }),
     handler: async (req, res) => {
         try {
-            const User = await Users.findAll();
-            if (!User) {
-                return responseHandler.notFound(res, "User not found");
+            // First get all users
+            const allUsers = await Users.findAll();
+
+            // Filter users created by logged in user
+            const createdUsers = allUsers.filter(user => user.created_by === req.user.username);
+
+            if (!createdUsers || createdUsers.length === 0) {
+                return responseHandler.notFound(res, "No users found");
             }
-            return responseHandler.success(res, "User fetched successfully", User);
+
+            // Get role IDs of filtered users
+            const roleIds = createdUsers.map(user => user.role_id);
+
+            // Get roles for filtered users
+            const roles = await Role.findAll({
+                where: {
+                    id: roleIds,
+                    role_name: {
+                        [Op.notIn]: ['client', 'sub-client', 'super-admin']
+                    }
+                }
+            });
+
+            // Filter users based on valid roles
+            const validRoleIds = roles.map(role => role.id);
+            const finalUsers = createdUsers.filter(user => 
+                validRoleIds.includes(user.role_id) && 
+                user.role_id !== req.user.role_id
+            );
+
+            if (!finalUsers || finalUsers.length === 0) {
+                return responseHandler.notFound(res, "No users found with valid roles");
+            }
+
+            // Add role details to final users
+            const usersWithRoles = finalUsers.map(user => {
+                const userRole = roles.find(role => role.id === user.role_id);
+                return {
+                    ...user.toJSON(),
+                    Role: userRole
+                };
+            });
+
+            return responseHandler.success(res, "Users fetched successfully", usersWithRoles);
         } catch (error) {
             return responseHandler.error(res, error?.message);
         }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { getCreatedUsers } from '../controllers/userControllers/index.js';
 
+import { authenticateUser, checkRole } from '../middlewares/index.js';
 
 const router = express.Router();
+
+router.use(authenticateUser, checkRole);
 
 // Route to get users created by a specific user
 router.get('/', getCreatedUsers.validator, getCreatedUsers.handler);
